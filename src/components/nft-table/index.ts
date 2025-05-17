116f02fb91f8136d7db05daf29a407a3b0ff4fc0
get <torsten@ocoso.io>
2025-05-18 00:11:39 +0200

    refactor(nft-table): rename component files to follow PascalCase convention

--- src/components/nft-table/index.ts
+++ src/components/nft-table/index.ts
@@ -1,9 +1,5 @@
-// Dieser Einstiegspunkt exportiert die NFTTable-Komponente
-// und stellt sicher, dass sie als Web-Komponente registriert ist
+import { NFTTable } from './NFTTable';
 
-import { NFTTable } from './nft-table';
-
-// Nur einmal registrieren, falls die Datei mehrfach importiert wird
 if (!customElements.get('nft-table')) {
   customElements.define('nft-table', NFTTable);
 }
