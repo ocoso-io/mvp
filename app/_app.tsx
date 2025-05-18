51061501df71b694e432645be85cc21234dc5936
get <torsten@ocoso.io>
2025-05-18 17:31:17 +0200

    chore: remove custom _app and _document files

--- app/_app.tsx
+++
@@ -1,8 +0,0 @@
-import type { AppProps } from 'next/app';
-import { JSX } from 'react';
-import React from 'react';
-import './globals.css';
-
-export default function App({ Component, pageProps }: AppProps): JSX.Element {
-    return <Component {...pageProps} />;
-}
