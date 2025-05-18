7f27f0c2c0e5d15eea4f0cdc63e41ca38288ebcb
get <torsten@ocoso.io>
2025-05-18 16:31:05 +0200

    refactor(build): simplify tsup config and add separate build scripts for prod and dev

--- packages/wallet/tsup.config.ts
+++ packages/wallet/tsup.config.ts
@@ -1,21 +1,24 @@
-import { defineConfig } from 'tsup';
+import {defineConfig} from 'tsup';
 
-export default defineConfig({
-    entry: ['src/index.ts'],
-    format: ['cjs', 'esm'],
-    dts: {
-        compilerOptions: {
-            incremental: false,
-        }
-    },
-    splitting: false,
-    sourcemap: true,
-    clean: true,
-    minify: process.env.NODE_ENV === 'production',
-    external: ['react', 'react-dom'],
-    outExtension({ format }) {
-        return {
-            js: format === 'cjs' ? '.js' : '.mjs',
-        };
-    },
-});
+export default defineConfig((options) => {
+    return {
+        entry: ['src/index.ts'],
+        format: ['cjs', 'esm'],
+        dts: {
+            compilerOptions: {
+                incremental: false,
+            }
+        },
+        splitting: false,
+        sourcemap: true,
+        clean: true,
+        minify: options.minify,
+        external: ['react', 'react-dom'],
+        outExtension({format}) {
+            return {
+                js: format === 'cjs' ? '.js' : '.mjs',
+            };
+        },
+    }
+})
+;
